9c3e343bb075f73e425f7bef93a142120c70c810
Chance Hudson <jchancehud@gmail.com>
2018-09-09 17:53:49 -0500

    Add build system/npm

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/App.tsx'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'static'),
+    filename: 'bundle.js',
+  },
+};
